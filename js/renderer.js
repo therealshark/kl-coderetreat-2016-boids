c6f58ac84933c0dd39fbbabe314578ca4c70ad9b
nope <nope@nope.nope>
2016-10-28 12:26:24 +0200

    visuallization of the "angle"

--- js/renderer.js
+++ js/renderer.js
@@ -13,7 +13,19 @@ var Renderer = (function(){
     ctx.clearRect(0, 0, canvas.width, canvas.height);
     boids.forEach(boid => {
       ctx.fillStyle = "black";
-      ctx.fillRect(boid.position.x, boid.position.y, 10, 10);
+      ctx.strokeStyle = "red";
+      ctx.fillRect(boid.position.x - 2, boid.position.y - 2, 4, 4);
+      ctx.beginPath();
+      ctx.moveTo(boid.position.x, boid.position.y);
+      var direction = boid.position.add(boid.speed.multiplyScalar(20));
+      ctx.lineTo(direction.x, direction.y);
+      ctx.stroke();
+      var angle = boid.speed.angle();
+      ctx.strokeStyle = "blue";
+      ctx.beginPath();
+      ctx.moveTo(boid.position.x, boid.position.y);
+      ctx.lineTo(boid.position.x + Math.sin(angle) * 20, boid.position.y - Math.cos(angle) + 20);
+      ctx.stroke();
     });
   }
 
